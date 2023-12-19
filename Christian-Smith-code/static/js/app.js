7bb133f23b2924e2317aa4b547c9bf4f9f9fa918
christian-a-smith <christianasmith@utexas.edu>
2023-12-19 15:45:37 -0600

    uploading completed JS file and readme

---
+++ Christian-Smith-code/static/js/app.js
@@ -0,0 +1,103 @@
+//Use the D3 library to read in samples.json from the URL https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.
+const my_url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
+function init(){
+let selector = d3.select("#selDataset")
+
+//loop over "names" and create dropdown
+d3.json(my_url).then(function(data) {
+    console.log(data);
+    let datanames = data.names;
+    datanames.forEach((sample)=>{
+      selector.append("option").text(sample).property("value",sample)
+    });
+    //Populate page on startup
+   let startup = datanames[0];
+   getinfo(startup)
+   createallcharts(startup);
+  });
+};
+init();
+//Calls table and chart based on dropdown selection
+function optionChanged(newdata){
+  getinfo(newdata);
+  createallcharts(newdata);
+};
+//Create table
+function getinfo(userdata){
+d3.json(my_url).then(function(data){
+  let metadata=data.metadata;
+  let metaarray=metadata.filter(obj=>obj.id==userdata);
+  let metaresult = metaarray[0];
+  let info=d3.select("#sample-metadata");
+  info.html("")
+  Object.entries(metaresult).forEach(([key,value])=>{
+    info.append("h6").text(`${key}: ${value}`)
+  });
+});
+};
+//Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+//Use sample_values as the values for the bar chart.
+function createallcharts(userdata){
+  d3.json(my_url).then(function(data){
+    let metadata=data.metadata;
+    let metaarray=metadata.filter(obj=>obj.id==userdata);
+    let metaresult = metaarray[0];
+    let wfreq = metaresult.wfreq;
+    let sampledata=data.samples;
+    let samplearray=sampledata.filter(obj=>obj.id==userdata);
+    let sampleresult = samplearray[0];
+    let otu_ids = sampleresult.otu_ids;
+    let otu_labels = sampleresult.otu_labels;
+    let sample_values = sampleresult.sample_values;
+    let yticks = otu_ids.slice(0,10).map(otu_id=>`OTU ${otu_id}`).reverse()
+    var bardata = [{
+      type: 'bar',
+      x: sample_values.slice(0,10).reverse(),
+      y: yticks,
+      text: otu_labels.slice(0,10).reverse(),
+      orientation: 'h'
+    }];
+    Plotly.newPlot('bar', bardata);
+    var trace1 = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        color: otu_ids,
+        size: sample_values,
+        colorscale: "Blues"
+      }
+    };
+    var bubbledata = [trace1];
+    var bubblelayout = {
+      title: '',
+    };
+    Plotly.newPlot('bubble', bubbledata, bubblelayout);
+    var gaugedata = [
+      {
+        type: "indicator",
+        mode: "gauge+number",
+        value: wfreq,
+        title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
+        gauge: {
+          axis: { range: [null, 9], tickwidth: 1, tickcolor: "darkblue", dtick:1 },
+          bar: { color: "darkblue" },
+          bgcolor: "white",
+          borderwidth: 2,
+          bordercolor: "gray",
+        }
+      }
+    ];
+        var gaugelayout = {
+      width: 500,
+      height: 400,
+      margin: { t: 25, r: 25, l: 25, b: 25 },
+      paper_bgcolor: "lavender",
+      font: { color: "darkblue", family: "Arial" }
+    };
+    
+    Plotly.newPlot('gauge', gaugedata, gaugelayout);
+     
+  })
+};
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # belly-button-challenge
+Repository for Module 14: Belly-button challenge
+
+## Table of Contents
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Installing](#installing)
+- [Usage](#usage)
+- [Contributing](#contributing)
+## About
+This assignment for the interactive visualizations module tasks us with calling data from a URL that contains a JSON sample, create and populate a dropdown menu where you can select different individuals, and displays a bar, bubble, and gauge chart based on their information in the study. 
+## Installiing
+JS Code is under file "Christian-Smith-code".
+## Usage
+To operate, either launch the HTML code directly from the file, or access the website from the Github Pages link."
+## Contributing
+Main author: Christian Smith
+Also received support and suggestions from AskBCS user dsaw1121 and tutor Sharon.
